714a635b221b66cf49ed487c9fbaaf2566e712dd
Andon Mefford <andonmefford@outlook.com>
2023-09-25 21:08:35 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-function divideAndConquerSum(a) {
-    return a;
-}
+function divideAndConquerSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    } else {
+      let oneThird = Math.floor(arr.length / 3);
+      let twoThirds = Math.floor((2 * arr.length) / 3);
+  
+      const left = divideAndConquerSort(arr.slice(0, oneThird));
+      const middle = divideAndConquerSort(arr.slice(oneThird, twoThirds));
+      const right = divideAndConquerSort(arr.slice(twoThirds));
+  
+      return mergeThree(left, middle, right);
+    }
+  }
+  
+  function mergeThree(left, middle, right) {
+    let sortedArr = [];
+    while (left.length && middle.length && right.length) {
+      if (left[0] <= middle[0] && left[0] <= right[0]) {
+        sortedArr.push(left.shift());
+      } else if (middle[0] <= left[0] && middle[0] <= right[0]) {
+        sortedArr.push(middle.shift());
+      } else {
+        sortedArr.push(right.shift());
+      }
+    }
+    return [...sortedArr, ...left, ...middle, ...right];
+  }
